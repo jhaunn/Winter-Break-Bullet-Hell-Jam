3b0b35a0e68d3d658fdb3c51e4db3b662fa493e5
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-12-26 15:30:48 +0800

    Added Player Movement and Player Shoot

---
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Camera cam;
+
+    [SerializeField] private float moveSpeed;
+
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = FindObjectOfType<Camera>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 look = mousePos - rb.position;
+        float angle = Mathf.Atan2(look.y, look.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
---
+++ My project/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject[] shootPoints;
+    [SerializeField] private GameObject[] bullets;
+
+    [SerializeField] private float shootInterval;
+    private float currentShootInterval;
+    [SerializeField] private float shootForce;
+
+    private void Update()
+    {
+        currentShootInterval -= Time.deltaTime;
+
+        if (currentShootInterval <= 0f)
+        {
+            currentShootInterval = shootInterval;
+
+            foreach (GameObject shootPoint in shootPoints)
+            {
+                GameObject bullet = Instantiate(bullets[0], shootPoint.transform.position, shootPoint.transform.rotation);
+                bullet.GetComponent<Rigidbody2D>().AddForce(shootPoint.transform.up * shootForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
