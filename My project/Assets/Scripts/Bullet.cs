01debf68e10834c0373f91adb076ea8e31a46aa0
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-12-29 12:44:06 +0800

    Updated Bullet Attack and Collision

--- My project/Assets/Scripts/Bullet.cs
+++ My project/Assets/Scripts/Bullet.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void Start()
+    private bool fromPlayer;
+
+    public void IsFromPlayer(bool fromPlayer)
     {
-        Destroy(gameObject, 10f);
+        this.fromPlayer = fromPlayer;
+    }
+
+    public void DestroyBullet(float value)
+    {
+        Destroy(gameObject, value);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (fromPlayer)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- My project/Assets/Scripts/ShootBullets.cs
+++ My project/Assets/Scripts/ShootBullets.cs
@@ -7,9 +7,11 @@ public class ShootBullets : MonoBehaviour
     [SerializeField] private GameObject[] shootPoints;
     [SerializeField] private GameObject[] bullets;
 
+    [SerializeField] private bool isPlayer;
     [SerializeField] private float shootInterval;
     private float currentShootInterval;
     [SerializeField] private float shootForce;
+    [SerializeField] private float bulletLife;
 
     private void Start()
     {
@@ -26,7 +28,10 @@ public class ShootBullets : MonoBehaviour
             foreach (GameObject shootPoint in shootPoints)
             {
                 GameObject bullet = Instantiate(bullets[0], shootPoint.transform.position, shootPoint.transform.rotation);
+
+                bullet.GetComponent<Bullet>().IsFromPlayer(isPlayer);
                 bullet.GetComponent<Rigidbody2D>().AddForce(shootPoint.transform.up * shootForce, ForceMode2D.Impulse);
+                bullet.GetComponent<Bullet>().DestroyBullet(bulletLife);
             }
         }
     }
